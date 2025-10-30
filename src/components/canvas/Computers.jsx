c73fd3df3a517d7d612f97a0c66c25cd49f0c157
Tomas <tomaslonnblad@gmail.com>
2025-10-30 18:50:40 +0100

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -64,7 +64,7 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop='demand'
       shadows
-      dpr={[2, 2]}
+      dpr={[2, 4]}
       camera={{ position: [0.1, 0.1, 0.1], fov: 80 }}
       gl={{ preserveDrawingBuffer: true }}
     >
